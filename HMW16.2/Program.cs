61b75a315bb6a1b9eb677fc594dd13878050d72c
Automation <automation@local>
2026-04-29 03:28:48 +0300

    completed 16.2 homework

---
+++ HMW16.2/Program.cs
@@ -0,0 +1,12 @@
+﻿while (true)
+{
+    Console.Write("Enter a number:");
+    string input = Console.ReadLine();
+
+    if (int.TryParse(input, out int number))
+    {
+        Console.WriteLine($"You entered: {number}");
+        break;
+    }
+    Console.WriteLine($"{input} is not a valid number. Please try again.");
+}
