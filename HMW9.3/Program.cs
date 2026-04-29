fa8bd428a79c73662fe01247cc12b2a77dadbf6e
Automation <automation@local>
2026-04-29 04:00:56 +0300

    completed 9.3 homework

---
+++ HMW9.3/Program.cs
@@ -0,0 +1,5 @@
+﻿string input = "hello, world, foo, bar, baz";                                                                                                                                                                                          
+   
+string result = input.Replace(" ", "");                                                                                                                                                                                                
+                       
+Console.WriteLine(result);
