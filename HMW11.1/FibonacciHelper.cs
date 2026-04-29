808c71c33328d4a84946f5b503f1bd228556c718
Automation <automation@local>
2026-04-29 14:57:07 +0300

    Completed 11.1 homework

---
+++ HMW11.1/FibonacciHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace HMW11._1;
+
+public static class FibonacciHelper
+{
+    public static int Calculate(int n)
+    {                                                                                                                                                                                                                                      
+        if (n == 1) return 0;               
+        if (n == 2) return 1;                                                                                                                                                                                     
+                                                                                                                                                                                                                                         
+        return Calculate(n - 1) + Calculate(n - 2);                                                                                                                                                                     
+    } 
+}
