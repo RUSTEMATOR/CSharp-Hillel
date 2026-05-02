17f12b739294a9a8282c46541dac57ba33e111f3
Automation <automation@local>
2026-05-02 22:03:39 +0300

    completed HMW15.1

---
+++ HMW15.1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введіть шлях до вихідного файлу: ");                                                                                                                                                                                    
+string sourcePath = Console.ReadLine();                                                                                                                                                                                                
+                                                                  
+Console.Write("Введіть шлях до файлу, в який потрібно скопіювати дані: ");                                                                                                                                                             
+string destinationPath = Console.ReadLine();                    
+                                                                                                                                                                                                                                         
+string content = File.ReadAllText(sourcePath);                  
+File.WriteAllText(destinationPath, content);
+
+Console.WriteLine("Файл успішно скопійовано!");
